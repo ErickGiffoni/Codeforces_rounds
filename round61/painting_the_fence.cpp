2eb432ad98dda1311f01681edc61582ab3083995
ErickGiffoni <giffoni.erick@gmail.com>
2019-03-05 16:46:42 -0300

    inicio

---
+++ round61/painting_the_fence.cpp
@@ -0,0 +1,13 @@
+int main (){
+
+int n_sec , q_painters;
+int * li_ri = (int)calloc(q_painters*2);
+int painted_sec;
+
+scanf("%d %d", &n_sec, &q_painters);
+for(int i=1; i<q_painters; i++){
+  //scanf("%d %d", &li_ri[i-1], li_ri[i])
+}
+
+  return 0;
+}
